74c93dab15f25d9ec586978896ac40570fb0bca3
Tumlinh <tumlinh@protonmail.com>
2016-03-02 12:52:42 +0100

    Bidirectional telemetry Blink LED

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include "mbed.h"
+#include <string.h>
 #include "TFC.h"
 #include "Telemetry.h"
+
+struct TM_state {
+	float throttle = 0.0;
+	float direction = 0.0;
+	uint8_t tlm_frames[3] = {0, 0, 0};
+	uint16_t image = 0;
+	int16_t deriv_image = 0;
+};
+
+void process(TM_state *state, TM_msg *msg);
 
 int main()
 {
@@ -8,36 +19,48 @@ int main()
 	float avg_brightness = 0.0;
 	int exposure_time_us = 8000;
 	int16_t deriv_image[128];
-	int frames = 0, fps = 0;
+	int cam_frames = 0, fps = 0;
 	int ticks = 0;
-	int display_interval = 100;
+	uint8_t info_interval = 10;
+	bool bLed = false;
 	
-	Timer tm_tick, tm_fps_upd, tm_servo_upd, tm_motor_upd, tm_camera, tm_display, tm_tlm;
+	Timer tm_tick, tm_fps_upd, tm_servo_upd, tm_motor_upd, tm_camera, tm_info, tm_tlm, tm_led;
 	
 	// Initialize modules
 	TFC_Init();
 	
 	//TFC_SetLineScanExposureTime(40000);
 	
-	//Serial pc(USBTX, USBRX);
-	//pc.baud(115200);
-	
+	// Initialize telemetry
 	Telemetry TM(115200);
+	TM_state state;
+	TM.sub(process, &state);
+	
+	// Activate the engines
+	TFC_HBRIDGE_ENABLE;
 	
 	// Initialize timers
 	tm_fps_upd.start();
 	tm_servo_upd.start();
 	tm_motor_upd.start();
 	//tm_camera.start();
-	tm_display.start();
+	tm_info.start();
 	tm_tlm.start();
+	tm_led.start();
 	
 	while (1) {
+		// Blink
+		if (tm_led.read_ms() > 500) {
+			tm_led.reset();
+			TFC_SetBatteryLED_Level(bLed);
+			bLed = !bLed;
+		}
+		
 		// Servo mode 
 		if (TFC_GetDIP_Switch() &0x01 && tm_servo_upd.read_ms() > 10) {
 			tm_servo_upd.reset();
-			servo_pos = TFC_ReadPot(0);
-			TFC_SetServo(0, servo_pos);
+			// TODO: check value of direction?
+			TFC_SetServo(0, state.direction);
 		}
 		
 		// Camera mode
@@ -47,7 +70,7 @@ int main()
 			
 			// Derive frame
 			uint32_t buffer = 0;
-			for (int i = 0; i < 127; i++) {
+			for (uint8_t i = 0; i < 127; i++) {
 				deriv_image[i] = TFC_LineScanImage0[i + 1] - TFC_LineScanImage0[i];
 				buffer += TFC_LineScanImage0[i];
 			}
@@ -56,17 +79,11 @@ int main()
 			deriv_image[127] = deriv_image[126];
 			buffer += TFC_LineScanImage0[127];
 			
-			// Publish data
-			for (int i = 0; i < 127; i++) {
-				//TM.pub_u8("cam", TFC_LineScanImage0[i]);
-				TM.pub_i8("cam2", deriv_image[i]);
-			}
+			// Update telemetry data
+			state.image = TFC_LineScanImage0[63];
+			state.deriv_image = deriv_image[63];
 			
-			frames++;
-			
-			// Compute average brightness and display it with LEDs
-			avg_brightness = ((float) buffer) / 128.0;
-			TFC_SetBatteryLED_Level((avg_brightness + 1) / 1024);
+			cam_frames++;
 		}
 		
 		// Motor mode
@@ -75,24 +92,55 @@ int main()
 			// TODO
 		}
 		
-		// Display debug information
-		if (tm_display.read_ms() > display_interval) {
-			tm_display.reset();
-			float avg_tick_t = (float) display_interval / (float) ticks * 1000.0;
-			//pc.printf("t: %dus   CAM: %d   FPS: %d\n", (int) avg_tick_t,
-			//(int) avg_brightness, fps);
+		// Receive and send telemetry data
+		if (tm_info.read_ms() > info_interval) {
+			tm_info.reset();
+			
+			// Receive data
+			TM.update();
+			
+			// Update variables
+			//TFC_SetServo(0, state.direction);
+			
+			// Send data
+			TM.pub_f32("bat", TFC_ReadBatteryVoltage());
+			TM.pub_u16("cam", state.image);
+			TM.pub_i16("cam2", state.deriv_image);
 			TM.pub_u8("fps", (uint8_t) fps);
-			TM.pub_u8("tick", (uint8_t) avg_tick_t);
+			TM.pub_f32("direction", state.direction);
+			
+			float avg_tick_t = (float) info_interval / (float) ticks * 1000.0;
 			ticks = 0;
+			TM.pub_u8("tick", (uint8_t) avg_tick_t);
+			
+			TM.pub_u8("tlm0", (uint8_t) state.tlm_frames[0]);
 		}
 		
 		// Update average fps
 		if (tm_fps_upd.read_ms() > 1000) {
 			tm_fps_upd.reset();
-			fps = frames;
-			frames = 0;
+			fps = cam_frames;
+			cam_frames = 0;
 		}
 		
 		ticks++;
 	}
+}
+
+void process(TM_state *state, TM_msg *msg)
+{
+	state->tlm_frames[0]++;
+	if (msg->type == TM_float32) {
+		float buffer;
+		if (strcmp("throttle", msg->topic) == 0) {
+			if (emplace_f32(msg, &buffer)) {
+				state->throttle = buffer;
+			}
+		}
+		else if (strcmp("direction", msg->topic) == 0) {
+			if (emplace_f32(msg, &buffer)) {
+				state->direction = buffer;
+			}
+		}
+        }
 }
