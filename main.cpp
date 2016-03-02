f79393408e7ebd50d9d0184625eda47016a1271c
Tumlinh <tumlinh@protonmail.com>
2016-03-02 01:18:17 +0100

    Testing Telemetry

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "mbed.h"
 #include "TFC.h"
+#include "Telemetry.h"
 
 int main()
 {
@@ -11,34 +12,37 @@ int main()
 	int ticks = 0;
 	int display_interval = 100;
 	
-	Timer t_tick, t_fps_upd, t_servo_upd, t_motor_upd, t_camera, t_display;
+	Timer tm_tick, tm_fps_upd, tm_servo_upd, tm_motor_upd, tm_camera, tm_display, tm_tlm;
 	
 	// Initialize modules
 	TFC_Init();
 	
 	//TFC_SetLineScanExposureTime(40000);
 	
-	Serial pc(USBTX, USBRX);
-	pc.baud(115200);
+	//Serial pc(USBTX, USBRX);
+	//pc.baud(115200);
+	
+	Telemetry TM(115200);
 	
 	// Initialize timers
-	t_fps_upd.start();
-	t_servo_upd.start();
-	t_motor_upd.start();
-	t_camera.start();
-	t_display.start();
+	tm_fps_upd.start();
+	tm_servo_upd.start();
+	tm_motor_upd.start();
+	//tm_camera.start();
+	tm_display.start();
+	tm_tlm.start();
 	
 	while (1) {
 		// Servo mode 
-		if (TFC_GetDIP_Switch() &0x01 && t_servo_upd.read_ms() > 10) {
-			t_servo_upd.reset();
+		if (TFC_GetDIP_Switch() &0x01 && tm_servo_upd.read_ms() > 10) {
+			tm_servo_upd.reset();
 			servo_pos = TFC_ReadPot(0);
 			TFC_SetServo(0, servo_pos);
 		}
 		
 		// Camera mode
 		if (TFC_GetDIP_Switch() &0x02 && TFC_LineScanImageReady == 1) {
-			t_camera.reset();
+			//tm_camera.reset();
 			TFC_LineScanImageReady = 0;
 			
 			// Derive frame
@@ -52,6 +56,12 @@ int main()
 			deriv_image[127] = deriv_image[126];
 			buffer += TFC_LineScanImage0[127];
 			
+			// Publish data
+			for (int i = 0; i < 127; i++) {
+				//TM.pub_u8("cam", TFC_LineScanImage0[i]);
+				TM.pub_i8("cam2", deriv_image[i]);
+			}
+			
 			frames++;
 			
 			// Compute average brightness and display it with LEDs
@@ -60,23 +70,25 @@ int main()
 		}
 		
 		// Motor mode
-		if (TFC_GetDIP_Switch() &0x04 && t_motor_upd.read_ms() > 10) {
-			t_motor_upd.reset();
+		if (TFC_GetDIP_Switch() &0x04 && tm_motor_upd.read_ms() > 10) {
+			tm_motor_upd.reset();
 			// TODO
 		}
 		
 		// Display debug information
-		if (t_display.read_ms() > display_interval) {
-			t_display.reset();
+		if (tm_display.read_ms() > display_interval) {
+			tm_display.reset();
 			float avg_tick_t = (float) display_interval / (float) ticks * 1000.0;
-			pc.printf("t: %dus   CAM: %d   FPS: %d\n", (int) avg_tick_t,
-			(int) avg_brightness, fps);
+			//pc.printf("t: %dus   CAM: %d   FPS: %d\n", (int) avg_tick_t,
+			//(int) avg_brightness, fps);
+			TM.pub_u8("fps", (uint8_t) fps);
+			TM.pub_u8("tick", (uint8_t) avg_tick_t);
 			ticks = 0;
 		}
 		
 		// Update average fps
-		if (t_fps_upd.read_ms() > 1000) {
-			t_fps_upd.reset();
+		if (tm_fps_upd.read_ms() > 1000) {
+			tm_fps_upd.reset();
 			fps = frames;
 			frames = 0;
 		}
