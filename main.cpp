fba1fcf4ffc611fa12babfd019553b430a709c01
Tumlinh <tumlinh@protonmail.com>
2016-03-10 17:31:55 +0100

    Detection of peaks to deduce position of lines

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "mbed.h"
 #include "TFC.h"
 #include "Telemetry.h"
+#include <vector>
+#define FRAME_SIZE 96
+#define UNDETECTED -1
 
 struct TM_state {
 	float throttle = 0.0;
 	float direction = 0.0;
 	uint8_t tlm_frames[3] = {0, 0, 0};
+};
+
+struct Peak {
+	uint8_t left;
+	uint8_t right;
 };
 
 void process(TM_state *state, TM_msg *msg);
@@ -15,9 +23,10 @@ int main()
 	float servo_pos = 0.0;
 	float avg_brightness = 0.0;
 	int exposure_time_us = 8000;
-	uint16_t image[128];
-	int16_t deriv_image[128];
-	float y[128];
+	uint16_t image[FRAME_SIZE];
+	int16_t deriv_image[FRAME_SIZE];
+	float filtered_image[FRAME_SIZE];
+	uint16_t peak_threshold = 100;
 	int cam_frames = 0, fps = 0;
 	int ticks = 0;
 	uint16_t info_interval = 100;
@@ -67,31 +76,94 @@ int main()
 			//tm_camera.reset();
 			TFC_LineScanImageReady = 0;
 			
-			// Copy image to buffer
-			for (uint8_t i = 0; i < 128; i++) {
-				image[i] = TFC_LineScanImage0[i];
+			// Copy image to buffer (remove first and last 16 pixels)
+			for (uint8_t i = 0; i < FRAME_SIZE; i++) {
+				image[i] = TFC_LineScanImage0[i + 16];
 			}
 			
-			// Derive frame
-			for (uint8_t i = 0; i < 127; i++) {
+			// TODO: Filter image?
+			
+			// Derive image
+			for (uint8_t i = 0; i < FRAME_SIZE - 1; i++) {
 				deriv_image[i] = image[i + 1] - image[i];
 			}
 			
 			// Correct side effect
-			deriv_image[127] = deriv_image[126];
+			deriv_image[FRAME_SIZE - 1] = deriv_image[FRAME_SIZE - 2];
 			
-			// Filter derivative frame (IIR low-pass filter)
+			// Filter derivative image (IIR low-pass filter)
 			float b[] = {0.1439739168, 0.2879478337, 0.1439739168};
 			float a[] = {0, -0.2809457379, 0.1855605367};
-			y[0] = 0;
-			y[1] = 0;
-			for (uint8_t n = 2; n < 128; n++) {
-				y[n] = 0;
-				for (uint8_t i = 0; i < sizeof(b) / sizeof(*b); i++)
-					y[n] += b[i] * deriv_image[n - i];
-				for (uint8_t i = 0; i < sizeof(a) / sizeof(*a); i++)
-					y[n] -= a[i] * y[n - i];
+			filtered_image[0] = 0;
+			filtered_image[1] = 0;
+			for (uint8_t n = 2; n < FRAME_SIZE; n++) {
+				filtered_image[n] = 0;
+				for (uint8_t i = 0; i < 3; i++)
+					filtered_image[n] += b[i] * deriv_image[n - i];
+				for (uint8_t i = 0; i < 3; i++)
+					filtered_image[n] -= a[i] * filtered_image[n - i];
 			}
+			
+			// TODO: Resample filtered image at higher frequency?
+			
+			// Detect peaks to deduce positions of left and right lines
+			std::vector<Peak> pos_peaks, neg_peaks;
+			bool in_pp = false, in_np = false;
+			for (uint8_t i = 2; i < FRAME_SIZE; i++) {
+				Peak peak;
+				uint8_t right;
+				if (in_pp) {
+					// Detect right of positive peak
+					if (filtered_image[i] >= peak_threshold
+					    && filtered_image[i] < filtered_image[i - 1]) {
+						right = i;
+					}
+					// Detect end of positive peak
+					if (filtered_image[i] <= 0) {
+						in_pp = false;
+						peak.right = right;
+						pos_peaks.push_back(peak);
+					}
+				}
+				else if (in_np) {
+					// Detect right of negative peak
+					if (filtered_image[i] <= - peak_threshold
+					    && filtered_image[i] > filtered_image[i - 1]) {
+						right = i;
+					}
+					// Detect end of negative peak
+					if (filtered_image[i] >= 0) {
+						in_np = false;
+						peak.right = right;
+						neg_peaks.push_back(peak);
+					}
+				}
+				else {
+					// Detect start of positive peak
+					if (filtered_image[i] >= peak_threshold
+					    && filtered_image[i] > filtered_image[i - 1]) {
+						peak.left = i;
+						in_pp = true;
+					}
+					// Detect start of negative peak
+					else if (filtered_image[i] <= - peak_threshold
+					    && filtered_image[i] < filtered_image[i - 1] && !a) {
+						peak.left = i;
+						in_np = true;
+					}
+				}
+			}
+			
+			// Deduce positions of left and right lines
+			// TODO
+			int8_t lline_pos = UNDETECTED, rline_pos = UNDETECTED;
+			if (pos_peaks.size() == 1 && neg_peaks.size() == 0)
+				lline_pos = pos_peaks[0].right;
+			else if (pos_peaks.size() == 0 && neg_peaks.size() == 1)
+				rline_pos = neg_peaks[0].left;
+			else if (pos_peaks.size() == 1 && neg_peaks.size() == 1
+			         && neg_peaks[0].left < pos_peaks[0].left)
+			else
 			
 			cam_frames++;
 		}
@@ -127,7 +199,7 @@ int main()
 			char topic2 [] = "camd:000";
 			char topic3 [] = "camf:000";
 			if (bPB && !bPB_old) {
-				for (uint8_t i = 0; i < 128; i++) {
+				for (uint8_t i = 0; i < FRAME_SIZE; i++) {
 					sprintf(topic1, "cam:%u", i);
 					TM.pub_i16("cam", image[i]);
 					wait_ms(1);
@@ -135,7 +207,7 @@ int main()
 					TM.pub_i16("camd", deriv_image[i]);
 					wait_ms(1);
 					sprintf(topic3, "camf:%u", i);
-					TM.pub_f32("camf", y[i]);
+					TM.pub_f32("camf", filtered_image[i]);
 					wait_ms(1);
 				}
 				/*for (uint8_t i = 0; i < 128; i++) {
@@ -172,5 +244,5 @@ void process(TM_state *state, TM_msg *msg)
 		else if (strcmp("direction", msg->topic) == 0)
 			if (emplace_f32(msg, &buffer))
 				state->direction = buffer;
-        }
+	}
 }
